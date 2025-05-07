0ad1b1155110e21ac83e47d739af4589ca369335
Theo-D <theodessaigne@hotmail.fr>
2025-05-07 15:57:34 +0200

    Added main guard for preventing execution on import

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 import variable_load_5
 
-variables = []
 
-for name in dir(variable_load_5):
-    if name.startswith('__'):
-        continue
-    var = getattr(variable_load_5, name)
-    if not callable(var):
-        variables.append(name)
+def main():
+    variables = []
 
-for varName in variables:
-    val = getattr(variable_load_5, varName)
-    print("{}".format(val))
+    for name in dir(variable_load_5):
+        if name.startswith('__'):
+            continue
+        var = getattr(variable_load_5, name)
+        if not callable(var):
+            variables.append(name)
+
+    for varName in variables:
+        val = getattr(variable_load_5, varName)
+        print("{}".format(val))
+
+
+if __name__ == "__main__":
+    main()
