405277b8838cce6349f58c93126648ba67b59484
Theo-D <theodessaigne@hotmail.fr>
2025-05-07 11:23:34 +0200

    added end parameter for correct output length

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,7 +3,7 @@ def print_last_digit(number):
     if number < 0:
         number *= -1
         number %= 10
-        print("{:d}".format(number * -1))
+        print("{:d}".format(number * -1), end="")
     else:
         number %= 10
-        print("{:d}".format(number))
+        print("{:d}".format(number), end="")
