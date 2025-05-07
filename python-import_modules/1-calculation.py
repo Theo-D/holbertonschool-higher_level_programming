de9b611a81a31edae993d58cf35929039b9c12dc
Theo-D <theodessaigne@hotmail.fr>
2025-05-07 16:04:49 +0200

    changed import line to import every function from module

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-import calculator_1
+from calculator_1 import add, sub, mul, div
 
 
 def main():
     a = 10
     b = 5
 
-    print(f"{a} + {b} = {calculator_1.add(a, b)}")
-    print(f"{a} - {b} = {calculator_1.sub(a, b)}")
-    print(f"{a} * {b} = {calculator_1.mul(a, b)}")
-    print(f"{a} / {b} = {calculator_1.div(a, b)}")
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {sub(a, b)}")
+    print(f"{a} * {b} = {mul(a, b)}")
+    print(f"{a} / {b} = {div(a, b)}")
 
 
 if __name__ == "__main__":
