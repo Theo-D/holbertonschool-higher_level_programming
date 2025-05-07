e5a797583ae9be8b7ab944a3bbd7918d2983976c
Theo-D <theodessaigne@hotmail.fr>
2025-05-07 11:51:36 +0200

    Declared newStr before assignement

--- python-if_else_loops_functions/101-remove_char_at.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def remove_char_at(str, n):
-    i = 0
-    for char in str:
-        if i != n:
-            newStr += char
-           # print("{}".format(char), end="")
-        i = i + 1
-    return newStr
