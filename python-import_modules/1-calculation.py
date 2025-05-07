776ceff9d433efc964baeee4246de24c3ca39612
Theo-D <theodessaigne@hotmail.fr>
2025-05-07 16:02:26 +0200

    Added main guard for preventing execution on import

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 import calculator_1
 
-a = 10
-b = 5
 
-print(f"{a} + {b} = {calculator_1.add(a, b)}")
-print(f"{a} - {b} = {calculator_1.sub(a, b)}")
-print(f"{a} * {b} = {calculator_1.mul(a, b)}")
-print(f"{a} / {b} = {calculator_1.div(a, b)}")
+def main():
+    a = 10
+    b = 5
+
+    print(f"{a} + {b} = {calculator_1.add(a, b)}")
+    print(f"{a} - {b} = {calculator_1.sub(a, b)}")
+    print(f"{a} * {b} = {calculator_1.mul(a, b)}")
+    print(f"{a} / {b} = {calculator_1.div(a, b)}")
+
+
+if __name__ == "__main__":
+    main()
