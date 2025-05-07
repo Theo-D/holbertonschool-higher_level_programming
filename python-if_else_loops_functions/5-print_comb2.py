dbf70e89551adf5c4271f2d29e47fbf286c7aef5
Theo-D <theodessaigne@hotmail.fr>
2025-05-07 10:38:34 +0200

    Added import for itertool, to create a nested loop in one line

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        if i < 9 or j < 9:
-            print("{:d}{:d},".format(i, j), end=" ")
-        else:
-            print("{:d}{:d}".format(i, j))
+import itertools
+for i, j in itertools.product(range(0, 10), range(0, 10)):
+    if i < 9 or j < 9:
+        print("{:d}{:d},".format(i, j), end=" ")
+    else:
+        print("{:d}{:d}".format(i, j))
