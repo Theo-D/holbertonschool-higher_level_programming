44f09ea658ba0ba751849b8439ff51e6d85b1668
Theo-D <theodessaigne@hotmail.fr>
2025-05-07 15:59:21 +0200

    Added main guard for preventing execution on import

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 from sys import argv
 
-argc = len(argv) - 1
 
-if argc == 0:
-    print("0 arguments.")
-elif argc < 2:
-    print("{} argument:".format(argc))
-else:
-    print("{} arguments:".format(argc))
+def main():
+    argc = len(argv) - 1
 
-for i in range(argc):
-    print("{:d}: {}".format(i + 1, argv[i + 1]))
+    if argc == 0:
+        print("0 arguments.")
+    elif argc < 2:
+        print("{} argument:".format(argc))
+    else:
+        print("{} arguments:".format(argc))
+
+    for i in range(argc):
+        print("{:d}: {}".format(i + 1, argv[i + 1]))
+
+
+if __name__ == "__main__":
+    main()
