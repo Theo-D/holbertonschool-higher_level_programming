94652886483bdbbb732b527def76becb92d26092
Theo-D <theodessaigne@hotmail.fr>
2025-05-07 11:16:12 +0200

    changed function to print instead of returning

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    number %= 10
-    return number
+    if number < 0:
+        number *= -1
+        number %= 10
+        print("{}".format(number * -1))
+    else:
+        number %= 10
+        print("{}".format(number))
