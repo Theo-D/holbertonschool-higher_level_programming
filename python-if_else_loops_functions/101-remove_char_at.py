14a2dbe08a0fd828a5d95da746c03b6d1c606da5
Theo-D <theodessaigne@hotmail.fr>
2025-05-07 11:41:41 +0200

    Added format specifier :c to print

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -3,5 +3,5 @@ def remove_char_at(str, n):
     i = 0
     for char in str:
         if i != n:
-            print("{}".format(char), end="")
+            print("{:c}".format(char), end="")
         i = i + 1
