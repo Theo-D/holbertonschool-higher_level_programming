e693d09ca26c4652f6e9710249988a35b67738f7
Theo-D <theodessaigne@hotmail.fr>
2025-05-07 16:00:40 +0200

    Added main guard for preventing execution on import

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 from sys import argv
 
-argc = len(argv) - 1
-res = 0
-for i in range(argc):
-    res += int(argv[i + 1])
-if argc == 0:
-    print("0")
-else:
-    print("{:d}".format(res))
+
+def main():
+    argc = len(argv) - 1
+    res = 0
+    for i in range(argc):
+        res += int(argv[i + 1])
+    if argc == 0:
+        print("0")
+    else:
+        print("{:d}".format(res))
+
+
+if __name__ == "__main__":
+    main()
