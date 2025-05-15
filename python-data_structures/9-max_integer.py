93987478ba3baf2892b6c383214ca62a0577ff95
Theo-D <theodessaigne@hotmail.fr>
2025-05-15 14:18:55 +0200

    Moved initialization of i after checking my_list is not empty

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    i = my_list[0]
     if len(my_list) == 0:
         return None
+    i = my_list[0]
     for num in my_list:
         if num > i:
             i = num
