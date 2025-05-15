d6354585984ec2070566d1adb9901cac29f857df
Theo-D <theodessaigne@hotmail.fr>
2025-05-15 13:42:31 +0200

    Added nested loop to print matrix using string.format

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for list in matrix:
-        print(list)
+        idx = 0
+        for i in list:
+            if idx != len(list) - 1:
+                print("{:d}".format(i), end=" ")
+                idx += 1
+            else:
+                print("{:d}".format(i), end ="\n")
