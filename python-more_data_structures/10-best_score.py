182d6005bc7ea1fa61eac1487e71b552f182ab8b
Theo-D <theodessaigne@hotmail.fr>
2025-05-15 15:24:06 +0200

    Added condition to handle empty dict

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     i = 0
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) < 1:
         return None
     for pair in a_dictionary:
         if a_dictionary[pair] > i:
