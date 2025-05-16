7cbfbfc6046c39384c4a74f5a2dc1038ef7d42ae
Theo-D <theodessaigne@hotmail.fr>
2025-05-16 10:54:50 +0200

    Changed execption keyword from valueError to Type Errorx

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,8 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return True
-    except ValueError:
+    except TypeError:
         print("\"{}\" is not an integer".format(value))
         return False
+    else:
+        print("\"{:d}\" is an integer".format(value))
