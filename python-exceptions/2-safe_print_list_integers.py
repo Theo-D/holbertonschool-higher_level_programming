cf0e068c368984e26f7456e68bc54fe7ba5058dc
Theo-D <theodessaigne@hotmail.fr>
2025-05-16 11:19:04 +0200

    Added exception for TypError, and removed handling of InedxError

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0
-    try:
-        for elm in my_list[:x]:
-            try:
-                print("{:d}".format(elm), end="")
-                i += 1
-            except ValueError:
-                pass
-    except IndexError:
-        return
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except (ValueError, TypeError):
+            pass
+        else:
+            count += 1
     print()
-    return i
+    return count
