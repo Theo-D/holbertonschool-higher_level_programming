7e5305659f02b09910741de766d11c51d334e936
Theo-D <theodessaigne@hotmail.fr>
2025-05-22 09:35:51 +0200

    Added doc describing file

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
+
+"""
+Module containing definition of a class representing a Square
+"""
+
+
 class Square:
-    """ class representing a square """
+    """class representing a square """
     pass
