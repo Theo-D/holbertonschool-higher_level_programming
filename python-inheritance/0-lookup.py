aaeb15b7253a86cb3d8347f36e156c859eec1d9d
Theo-D <theodessaigne@hotmail.fr>
2025-05-26 11:44:18 +0200

    Added two blank lines after module doc to comply to PEP8

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
 """
 This module contains a function for returning methods and attributes
 of a given object.
 """
+
+
 def lookup(obj):
     """
     A function that returns attributes and methods of a given object.
