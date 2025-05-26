15d8e5d13e486f0a8fb2b77e724b274286182fb6
Theo-D <theodessaigne@hotmail.fr>
2025-05-26 11:40:24 +0200

    Added 0-lookup.py that returns a list of available attributes and methods of an object. Added README.md to contains a description of 0-lookup.py

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """
+    A function that returns attributes and methods of a given object.
+    Returns a list of said attributes and methods
+    """
+    return dir(obj)
---
+++ python-inheritance/README.md
@@ -0,0 +1,4 @@
+# Python - Inheritance
+### This README will include descritpions for all .py files in this directory
+
+0-lookup.py - Returns the list of available attributes and methods of an object.
