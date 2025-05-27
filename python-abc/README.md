5ce3a10f9e2e436af3ddec125afa487b0af26cbc
Theo-D <theodessaigne@hotmail.fr>
2025-05-27 14:24:06 +0200

    Added task_00_abc.py and README.md that includes description for the file.

---
+++ python-abc/README.md
@@ -0,0 +1,4 @@
+# Python - Abstract Base Classes
+### This README will include descritpions for all .py files in this directory
+
+task_00_abc.py - Contains definition for abstract class Animal and Dog and Cat classes inheriting from Animal.
---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+task_00_abc
+Module for practicing with creating abstract classes and using duck typing
+"""
+
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    """
+    A class defining an abstraction of an animal
+    """
+    @abstractmethod
+    def sound(self):
+        """
+        Abstract method defining sound produced by ananimal
+        """
+        pass
+
+class Dog(Animal):
+    """
+    A class representing a dog
+    """
+    def sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    """
+    A class representing a dog
+    """
+    def sound(self):
+        return "Meow"
