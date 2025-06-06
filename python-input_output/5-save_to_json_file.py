c48f0aa49aa7eee48af2d803888961c8a7c77f20
Theo-D <theodessaigne@hotmail.fr>
2025-06-06 12:29:29 +0200

    Appended return to 5-save_to_json.py

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""
+5-save_to_json_file.py - Module containing function for serializing
+python object to python file
+"""
 import json
 
 
 def save_to_json_file(my_obj, filename):
+    """
+    Function for serializing an object from a python
+    """
     with open(filename, 'w', encoding="utf-8") as f:
-        f.write(json.dumps(my_obj))
+        toWrite = f.write(json.dumps(my_obj))
+    return toWrite
