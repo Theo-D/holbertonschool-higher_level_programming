335efeae314d49b663676b6e1edaf23a9ca138ac
Theo-D <theodessaigne@hotmail.fr>
2025-06-10 18:05:54 +0200

    Fixed pycodestyle and added documentation to py modules

--- restful_api/task_02_requests.py
+++ restful_api/task_02_requests.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+"""
+task_02_requests.py - A module for learning about the requests python library
+"""
 import requests
 import json
 import csv
 
+
 def fetch_and_print_posts():
+    """
+    fetch_and_print_posts - A function for printing get request status code
+    and retrieved header titles
+    """
     r = requests.get("https://jsonplaceholder.typicode.com/posts")
     dicts = r.json()
     if isinstance(r, requests.Response):
@@ -11,12 +19,18 @@ def fetch_and_print_posts():
         for dict in dicts:
             print(dict['title'])
 
+
 def fetch_and_save_posts():
+    """
+    fetch_and_save_posts - A function for serializing get request's
+    header's to csv file
+    """
     r = requests.get("https://jsonplaceholder.typicode.com/posts")
     dicts = r.json()
     if isinstance(r, requests.Response):
         with open('posts.csv', 'w', encoding="utf-8") as f:
             fieldnames = ['id', 'title', 'body']
-            writer = csv.DictWriter(f, fieldnames=fieldnames, extrasaction='ignore')
+            writer = csv.DictWriter(f, fieldnames=fieldnames,
+                                    extrasaction='ignore')
             writer.writeheader()
             writer.writerows(dicts)
--- restful_api/task_03_http_server.py
+++ restful_api/task_03_http_server.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
+"""
+task_03_http_server.py - A Module containing classes and functions to learn
+about http.server python library
+"""
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
-class myServer(BaseHTTPRequestHandler):
 
+class myServer(BaseHTTPRequestHandler):
+    """
+    myServer - A subclass of BaseHTTPRequestHandler to adapt its methods.
+    """
     def do_GET(self):
+        """
+        do_GET - A method handling get responses from web server.
+        """
         dataSet = {"name": "John", "age": 30, "city": "New York"}
-        infoSet = {"version": "1.0", "description": "A simple API built with http.server"}
-        endpointDict = {"/data" : dataSet, "/info" : infoSet, "/status" : "OK"}
+        infoSet = {"version": "1.0", "description":
+                   "A simple API built with http.server"}
+        endpointDict = {"/data": dataSet, "/info": infoSet, "/status": "OK"}
         if self.path in endpointDict:
             print(json.dumps(endpointDict[self.path]))
             self.send_response(200)
@@ -15,14 +26,17 @@ class myServer(BaseHTTPRequestHandler):
         else:
             print("Endpoint Not Found")
             self.send_response(404)
-
         self.end_headers()
 
 
 def run(server_class=HTTPServer, handler_class=myServer):
+    """
+    run - Function to run webserver and learn about http methods.
+    """
     server_address = ('', 8000)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
+
 
 if __name__ == "__main__":
     run()
