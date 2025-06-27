1f09f93f30f05df4f9d1b59310d481772df3be6d
Theo-D <theodessaigne@hotmail.fr>
2025-06-27 21:48:19 +0200

    changed cursor obj name sur cur to stick to the documentation

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -18,13 +18,13 @@ if __name__ == "__main__":
         passwd=password,
         db=database
     )
-    cnx = db.cursor()
-    cnx.execute("SELECT cities.id, cities.name, states.name FROM cities "
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities "
                 "JOIN states ON cities.state_id = states.id ORDER BY "
                 "cities.id;")
 
-    for row in cnx.fetchall():
+    for row in cur.fetchall():
         print(row)
 
-    cnx.close()
+    cur.close()
     db.close()
